6322c738c98d907e7072e8bb4bf301378a573b0e
drbackend <bardia.backend@gmail.com>
2020-07-14 23:42:55 +0430

    blog-Tuesday-Jul-14-2020-23-42-55

--- src/controller/user/delete.go
+++ src/controller/user/delete.go
@@ -4,6 +4,7 @@ import (
 
 	"fmt"
 	"net/http"
+	"encoding/json"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/gorilla/mux"
 	"github.com/mrbardia72/blog-gorm-gorilla/src/model"
@@ -21,7 +22,7 @@ func Deleteuser(w http.ResponseWriter, r *http.Request) {
 	err := db.Where("name = ?", name).Find(&user).Error
 
 	if err != nil {
-		
+
 		fmt.Fprintf(w, "not exisits record for delete")
 		info:=" not exisits record for delete "
 		helpers.GetTimeDate(info)
@@ -30,6 +31,7 @@ func Deleteuser(w http.ResponseWriter, r *http.Request) {
 
 		db.Delete(&user)
 		info:=" Successfully Deleted User "
+		json.NewEncoder(w).Encode(info)
 		helpers.GetTimeDate(info)
 		fmt.Fprintf(w, "Successfully Deleted User")
 	}
