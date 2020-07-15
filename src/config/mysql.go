fe9b66e16dbfa3b6d19283ecbe5b972b177afb65
drbackend <bardia.backend@gmail.com>
2020-07-15 12:41:38 +0430

    blog-Wednesday-Jul-15-2020-12-41-38

--- src/config/mysql.go
+++ src/config/mysql.go
@@ -7,14 +7,14 @@ import (
 
 func MySql()(*gorm.DB)  {
 
-	dbDriver:="mysql"
-	dbName:="digi"
-	dbUser:="root"
-	dbPassword:="Bardi@1993"
+	dbDriver	:=	"mysql"
+	dbName		:=	"digi"
+	dbUser		:=	"root"
+	dbPassword	:=	"Bardi@1993"
 
 	db, err := gorm.Open(dbDriver, dbUser+":"+dbPassword+"@/"+dbName+"?charset=utf8&parseTime=True&loc=Local")
-	if err != nil {
-		panic("failed to connect database")
-	}
+		if err != nil {
+			panic("failed to connect database")
+		}
 	return db
 }
