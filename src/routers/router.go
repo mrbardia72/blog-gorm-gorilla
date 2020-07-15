b748f2cc42abd0130249cc20976086324a508e72
drbackend <bardia.backend@gmail.com>
2020-07-15 10:22:14 +0430

    blog-Wednesday-Jul-15-2020-10-22-14

--- src/routers/router.go
+++ src/routers/router.go
@@ -12,18 +12,19 @@ func HandleRequests() {
 
 	route := mux.NewRouter()
 
-	userroute := route.PathPrefix("/v1/api/u.er").Subrouter()
+	userroute := route.PathPrefix("/v1/api/user").Subrouter()
 	userroute.HandleFunc("/read", user.Alluser).Methods("GET")
 	userroute.HandleFunc("/delete/{name}", user.Deleteuser).Methods("DELETE")
 	userroute.HandleFunc("/update/{name}/{email}", user.Updateuser).Methods("PUT")
 	userroute.HandleFunc("/create/{name}/{email}", user.Newuser).Methods("POST")
 	userroute.HandleFunc("/search/{name}", user.Searchuser).Methods("GET")
 
-	postroute := route.PathPrefix("/v1/api/u.er").Subrouter()
-	postroute.HandleFunc("/posts", post.Allpost).Methods("GET")
+	postroute := route.PathPrefix("/v1/api/post").Subrouter()
+	postroute.HandleFunc("/read", post.Allpost).Methods("GET")
 	postroute.HandleFunc("/delete/{name}", post.Deletepost).Methods("DELETE")
 	postroute.HandleFunc("/update/{name}/{email}", post.Updatepost).Methods("PUT")
 	postroute.HandleFunc("/create/{name}/{email}", post.Newpost).Methods("POST")
 	postroute.HandleFunc("/search/{name}", post.Searchpost).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":8081", route))
 }
